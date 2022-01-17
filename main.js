b8de95fadf4820d08c1742de8e3ead6c99e30f48
Vitali Herasimenka <vitttold@inbox.ru>
2022-01-17 17:49:33 +0300

    5

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let questions = document.querySelectorAll(".questions-items");
+let questions = document.querySelectorAll(".questions-item");
 let dots = document.querySelectorAll(".question-dot");
 let answers = document.querySelectorAll(".answer");
-let answersActive = document.querySelector(".question-dot-active");
+
 
 function questionOpen(e) {
     let answerNumber = e.currentTarget.classList[0];
@@ -15,23 +15,32 @@ function questionOpen(e) {
         dots[answerNumber].classList.remove("question-dot");
         dots[answerNumber].classList.add("question-dot-active");
     }
-    console.log(answersActive)
 
-   }
+    console.log()
 
- function questionClose() {
-        answersActive.classList.remove("question-dot-active");
-        answersActive.classList.add("question-dot");
-        
-        console.log("oga")
+}
 
- }
-
-
-    for (let i = 0; i < questions.length; i++) {
-        questions[i].addEventListener('click', questionOpen);
+function questionClose(e) {
+    let answerNumber = e.currentTarget.classList[0];
+    for (let i = 0; i < answers.length; i++) {
+        if (dots[answerNumber].classList.contains('question-dot-active')) {
+            dots[i].classList.remove("question-dot-active");
+            dots[i].classList.add("question-dot");
+            answers[answerNumber].classList.add("answer-close");
+            answers[answerNumber].classList.remove("answer-open");
+        }
     }
-        answersActive.addEventListener('click', questionClose);
+
+}
+
+
+for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('click', questionOpen);
+}
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', questionClose);
+}
 
 
 
